41855040abc05261ac0181c4fe272e67b8ccfd2f
Yann Briançon <briyan@sfr.fr>
2022-07-08 17:51:32 +0200

    Handle async middleware

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-/* eslint-disable-next-line no-underscore-dangle */
-const _require = require('esm')(module);
-
-module.exports = _require('./src/index').default;
+module.exports = require("./lib").default;
